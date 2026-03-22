fd1470863522840bab96d25707cac1b78566cfee
npgtb <nopgtb@gmail.com>
2026-03-22 10:28:19 +0200

    Refactored Maiden to be more maintainable

--- DBM-Karazhan-Frostmourne/MaidenOfRot.lua
+++ DBM-Karazhan-Frostmourne/MaidenOfRot.lua
@@ -3,46 +3,96 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("20250929220131")
 mod:SetCreatureID(354272)
-
 mod:SetEncounterID(924)
 mod:SetModelID(18720)
-
 mod:RegisterCombat("combat")
-mod:SetWipeTime(600)
+
+--Helper to build the event string
+local function EventString(event_name, ...)
+	--Set everything in table
+	local parts = {event_name}
+	for i = 1, select("#", ...) do
+		table.insert(parts, tostring(select(i, ...)))
+	end
+	-- Concat the table
+	return table.concat(parts, " ")
+end
+
+--Possible difficulties of the fight
+local DIFFICULTY = {
+	NORMAL_10 = "normal10",
+	NORMAL_25 = "normal25",
+	HEROIC_10 = "heroic10",
+	HEROIC_25 = "heroic25"
+}
+
+--default to 25H difficulty for now
+local difficulty = DIFFICULTY.HEROIC_25
+local player_name = nil
+
+--Spell ids of the counter
+local SPELLS = {
+	DEEP_FREEZE = 72930,
+	FRENZY = 12795
+}
+
+--Timing table
+local TIMERS = {
+	[DIFFICULTY.NORMAL_10] = {
+		BERSERK = 600,
+		DEEP_FREEZE_CD = 30
+	},
+	[DIFFICULTY.NORMAL_25] = {
+		BERSERK = 600,
+		DEEP_FREEZE_CD = 30
+	},
+	[DIFFICULTY.HEROIC_10] = {
+		BERSERK = 600,
+		DEEP_FREEZE_CD = 30
+	},
+	[DIFFICULTY.HEROIC_25] = {
+		BERSERK = 600,
+		DEEP_FREEZE_CD = 30
+	},
+}
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 72930", -- Deep freeze
-	"SPELL_AURA_APPLIED 12795" -- Frenzy
+	EventString("SPELL_CAST_START", SPELLS.DEEP_FREEZE),
+	EventString("SPELL_AURA_APPLIED", SPELLS.FRENZY)
 )
 
 --Enrage timer
-local enrage_timer = mod:NewBerserkTimer(600)
+local enrage_timer = mod:NewBerserkTimer(TIMERS[difficulty].BERSERK)
 --Deep freeze target warning and timer
-local warning_targeted_deep_freeze	= mod:NewSpecialWarningYou(72930, nil, nil, nil, 1, 2)
-local timer_deep_freeze		= mod:NewCDTimer(30, 72930, nil, nil, nil, 2)
+local warning_targeted_deep_freeze = mod:NewSpecialWarningYou(SPELLS.DEEP_FREEZE, nil, nil, nil, 1, 2)
+local timer_deep_freeze	= mod:NewCDTimer(TIMERS[difficulty].DEEP_FREEZE_CD, SPELLS.DEEP_FREEZE, nil, nil, nil, 2)
 --Frenzy warning
-local warning_frenzy = mod:NewSpellAnnounce(12795, 3, nil, "Tank|Healer")
-
+local warning_frenzy = mod:NewSpellAnnounce(SPELLS.FRENZY, 3, nil, "Tank|Healer")
 
 function mod:OnCombatStart(delay)
-	enrage_timer:Start(-delay)
-	--timer_deep_freeze:Start()
+	--Fetch difficulty from dbm
+	difficulty = DBM:GetCurrentInstanceDifficulty() or DIFFICULTY.HEROIC_25
+	player_name = UnitName("player")
+	--Assume berserk ends it all
+	mod:SetWipeTime(TIMERS[difficulty].BERSERK)
+	--Begin timers
+	enrage_timer:Start(TIMERS[difficulty].BERSERK - delay)
+	timer_deep_freeze:Start(TIMERS[difficulty].DEEP_FREEZE_CD - delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	--Deep freeze casting
-	if args.spellId == 72930 then
+	if args.spellId == SPELLS.DEEP_FREEZE then
 		--Start scanning for the target and reset the cd timer. 15 scans at 0.05 interval
 		self:BossTargetScanner(args.sourceGUID, "deep_freeze_target_scan", 0.05, 15)
-		timer_deep_freeze:Start()
+		timer_deep_freeze:Start(TIMERS[difficulty].DEEP_FREEZE_CD)
 	end
 end
-
 
 function mod:deep_freeze_target_scan(targetname)
 	--Is the target us? if so show/play warning
 	if not targetname then return end
-	if targetname == UnitName("player") then
+	if targetname == player_name then
 		warning_targeted_deep_freeze:Show()
 		warning_targeted_deep_freeze:Play("targetyou")
 	end
@@ -50,7 +100,7 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	--Frenzy buff applied to the boss => massive melee haste gained
-	if args.spellId == 12795 then
+	if args.spellId == SPELLS.FRENZY then
 		warning_frenzy:Show()
 		warning_frenzy:Play("defensive")
 	end
