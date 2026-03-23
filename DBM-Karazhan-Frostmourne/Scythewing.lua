cb83d849ddcc700680c2e39733afd9b7a0f30b6c
npgtb <nopgtb@gmail.com>
2026-03-23 08:21:57 +0200

    Transitioned Scythewing spell data to support manual monitoring

--- DBM-Karazhan-Frostmourne/Scythewing.lua
+++ DBM-Karazhan-Frostmourne/Scythewing.lua
@@ -33,10 +33,10 @@ local player_guid = nil
 
 --Spell ids of the counter
 local SPELLS = {
-	DEADEN = 41410,
-	BLISTERING_COLD = 71047,
-	TAILSWEEP = 55696,
-	FROSTBOLT_VOLLEY = 72906
+	DEADEN = {NAME = "Deaden", ID = 41410},
+	BLISTERING_COLD = {NAME = "Blistering Cold", ID = 71047},
+	TAILSWEEP = {NAME = "Tail Sweep", ID = 55696},
+	FROSTBOLT_VOLLEY = {NAME = "Frostbolt Volley", ID = 72906}
 }
 
 --Timing table
@@ -72,23 +72,23 @@ local TIMERS = {
 }
 
 mod:RegisterEventsInCombat(
-	EventString("SPELL_CAST_START", SPELLS.DEADEN, SPELLS.BLISTERING_COLD),
-	EventString("SPELL_CAST_SUCCESS", SPELLS.TAILSWEEP, SPELLS.FROSTBOLT_VOLLEY),
-	EventString("SPELL_AURA_APPLIED", SPELLS.DEADEN)
+	EventString("SPELL_CAST_START", SPELLS.DEADEN.ID, SPELLS.BLISTERING_COLD.ID),
+	EventString("SPELL_CAST_SUCCESS", SPELLS.TAILSWEEP.ID, SPELLS.FROSTBOLT_VOLLEY.ID),
+	EventString("SPELL_AURA_APPLIED", SPELLS.DEADEN.ID)
 )
 
 --Enrage timer
 local enrage_timer = mod:NewBerserkTimer(TIMERS[difficulty].BERSERK)
 --Deaden warning and timer
-local deaden_warning = mod:NewSpecialWarningYou(SPELLS.DEADEN, nil, nil, nil, 1, 2)
-local timer_deaden = mod:NewCDTimer(TIMERS[difficulty].DEADEN_CD, SPELLS.DEADEN, nil, nil, nil, 2)
+local deaden_warning = mod:NewSpecialWarningYou(SPELLS.DEADEN.ID, nil, nil, nil, 1, 2)
+local timer_deaden = mod:NewCDTimer(TIMERS[difficulty].DEADEN_CD, SPELLS.DEADEN.ID, nil, nil, nil, 2)
 --Blistering Cold warning and timer
-local blistering_cold_warning = mod:NewSpecialWarningGTFO(SPELLS.BLISTERING_COLD, nil, nil, nil, 1, 8)
-local timer_blistering_cold = mod:NewCDTimer(TIMERS[difficulty].BLISTERING_COLD_CD, SPELLS.BLISTERING_COLD, nil, nil, nil, 2)
+local blistering_cold_warning = mod:NewSpecialWarningGTFO(SPELLS.BLISTERING_COLD.ID, nil, nil, nil, 1, 8)
+local timer_blistering_cold = mod:NewCDTimer(TIMERS[difficulty].BLISTERING_COLD_CD, SPELLS.BLISTERING_COLD.ID, nil, nil, nil, 2)
 --Frostbolt Volley CD timer
-local timer_frostbolt_volley = mod:NewCDTimer(TIMERS[difficulty].FROSTBOLT_VOLLEY_CD, SPELLS.FROSTBOLT_VOLLEY, nil, nil, nil, 2)
+local timer_frostbolt_volley = mod:NewCDTimer(TIMERS[difficulty].FROSTBOLT_VOLLEY_CD, SPELLS.FROSTBOLT_VOLLEY.ID, nil, nil, nil, 2)
 --Tailsweep CD timer
-local timer_tailsweep = mod:NewCDTimer(TIMERS[difficulty].TAILSWEEP_CD, SPELLS.TAILSWEEP, nil, nil, nil, 2)
+local timer_tailsweep = mod:NewCDTimer(TIMERS[difficulty].TAILSWEEP_CD, SPELLS.TAILSWEEP.ID, nil, nil, nil, 2)
 
 function mod:OnCombatStart(delay)
 	--Fetch difficulty from dbm
@@ -105,10 +105,10 @@ end
 
 function mod:SPELL_CAST_START(args)
 	--Deaden casting
-	if args.spellId == SPELLS.DEADEN then
+	if args.spellId == SPELLS.DEADEN.ID then
 		timer_deaden:Start(TIMERS[difficulty].DEADEN_CD)
 	--Blistering cold cast started
-    elseif args.spellId == SPELLS.BLISTERING_COLD then
+    elseif args.spellId == SPELLS.BLISTERING_COLD.ID then
 		timer_blistering_cold:Start(TIMERS[difficulty].BLISTERING_COLD_CD)
         blistering_cold_warning:Show()
         blistering_cold_warning:Play("runaway")
@@ -117,17 +117,17 @@ end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	-- Tailsweep
-	if args.spellId == SPELLS.TAILSWEEP then
+	if args.spellId == SPELLS.TAILSWEEP.ID then
 		timer_tailsweep:Start(TIMERS[difficulty].TAILSWEEP_CD)
 	-- Frostbolt volley
-    elseif args.spellId == SPELLS.FROSTBOLT_VOLLEY then
+    elseif args.spellId == SPELLS.FROSTBOLT_VOLLEY.ID then
 		timer_frostbolt_volley:Start(TIMERS[difficulty].FROSTBOLT_VOLLEY_CD)
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	-- Deaden applied to us
-	if args.spellId == SPELLS.DEADEN then
+	if args.spellId == SPELLS.DEADEN.ID then
 		if args.destName == player_name then
 			deaden_warning:Show()
 			deaden_warning:Play("targetyou")
